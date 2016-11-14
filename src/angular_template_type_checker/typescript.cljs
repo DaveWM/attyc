60fdb2086e5f0804bea946665840b1c6d92bcb33
David Martin <dwmartin41@gmail.com>
2016-11-14 00:07:34 +0000

    added support for filters

--- src/angular_template_type_checker/typescript.cljs
+++ src/angular_template_type_checker/typescript.cljs
@@ -1,5 +1,6 @@
 (ns angular-template-type-checker.typescript
-  (:require [cljs.nodejs :as node]))
+  (:require [cljs.nodejs :as node]
+            [clojure.string :as str]))
 
 (def ts-node (-> (node/require "ts-node")
                  (.register (clj->js {:project "./tsconfig.json"}))))
@@ -14,10 +15,14 @@
   ((get-compiler filename) code))
 
 (defn get-exprs-for-attr [[attr value]]
-  (let [ng-repeat-regex #"\S+\s+in\s+(\S+)(?:\s+track\s+by\s+\S+)?"]
-    (case attr
-      :ng-repeat (next (re-find ng-repeat-regex value))
-      [value])))
+  (let [ng-repeat-regex #"\S+\s+in\s+(.*)"]
+    (->> (case attr
+           :ng-repeat (rest (re-find ng-repeat-regex value)) ; remove the "x in" bit from the ng-repeat
+           [value])
+         (mapcat (fn [ng-expr]
+                   (let [[expr & filters] (str/split ng-expr "|")]
+                     (cons expr
+                           (mapcat #(rest (str/split % ":")) filters))))))))
 
 (defn build-typescript [attributes {:keys [model type import]}]
   (let [import-statement (when import (str "import {" type "} from '" import "';"))
